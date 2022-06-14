d66da5fc5beea61c50f3f1b13e44d31f2700f1f5
MrCubee <contact@mrcubee.fr>
2022-06-14 20:21:56 +0200

    Init repository.

---
+++ bukkit/src/main/java/net/liteskript/bukkit/LiteSkriptPlugin.java
@@ -0,0 +1,12 @@
+package net.liteskript.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LiteSkriptPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
---
+++ bungeecord/src/main/java/net/liteskript/bungee/LiteSkriptPlugin.java
@@ -0,0 +1,11 @@
+package net.liteskript.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class LiteSkriptPlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
